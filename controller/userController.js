8ce5c1c26fc7997f287ea9c3b39fbccffd05a2c5
sujal2617 <2200032771@kluniversity.in>
2024-11-29 01:34:21 +0530

    5th commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,40 +1,33 @@
-const UserModel = require('../models/userModel');
 const AWS = require('aws-sdk');
 
-// Check if the region is correctly set
-console.log('AWS region:', AWS.config.region);
-
-// Set your region
+// Set your region first
 AWS.config.update({ region: 'us-east-1' });
 
+// Now, create DynamoDB and S3 clients
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 
-
+// UserController logic follows
 const UserController = {
     getUser: async (req, res) => {
         try {
-            const data = await UserModel.getUserById(req.params.userId); res.status(200).json(data);
+            const data = await UserModel.getUserById(req.params.userId);
+            res.status(200).json(data);
         } catch (err) {
             res.status(500).json({ error: err.message });
         }
     },
-    createUser: async (req, res) =>
-    {
-        try
-        {
+    createUser: async (req, res) => {
+        try {
             const userData = req.body;
             await UserModel.createUser(userData);
             res.status(201).json({ message: "User created successfully" });
-        } catch (err)
-        {
+        } catch (err) {
             res.status(500).json({ error: err.message });
         }
     },
-    updateUser: async (req, res) =>
-    {
-        try
-        {
+    updateUser: async (req, res) => {
+        try {
             const userId = req.params.userId;
             const userData = req.body;
             await UserModel.updateUser(userId, userData);
@@ -43,14 +36,15 @@ const UserController = {
             res.status(500).json({ error: err.message });
         }
     },
-    deleteUser: async (req, res) =>
-    {
+    deleteUser: async (req, res) => {
         try {
             const userId = req.params.userId;
             await UserModel.deleteUser(userId);
             res.status(200).json({ message: "User deleted successfully" });
         } catch (err) {
-            res.status(500).json({ error: err.message }); }
+            res.status(500).json({ error: err.message });
+        }
     }
 };
+
 module.exports = UserController;
