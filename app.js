19a4e6e73e08f34861e245b83a3de0fbc8f057fa
sujal2617 <2200032771@kluniversity.in>
2024-11-29 01:08:53 +0530

    4rd commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page!');
+});
+
 // Set port and start server
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
